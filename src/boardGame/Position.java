8a5e03307e80f2393431641238a7e3f2e7787bcd
Julia <juliacpa73@gmail.com>
2024-03-06 19:10:50 -0300

    First Class: Position

---
+++ src/boardGame/Position.java
@@ -0,0 +1,33 @@
+package boardGame; //pacote correspondente a camada tabuleiro
+
+public class Position { // Classe-mãe Posição que indica a posição do tabuleiro
+    private int row; // Linha
+    private int column; // Coluna
+
+    public Position(int row, int column) {
+        this.row = row;
+        this.column = column;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+
+    public void setColumn(int column) {
+        this.column = column;
+    }
+
+    @Override
+    public String toString() { // toString para imprimir a posição na tela
+        return row + ", " + column + ".";
+    }
+
+}
